b22c8a5396f67c43fd1a619ae9e50ef7b7af426d
diogoacmarques <40327323+diogoacmarques@users.noreply.github.com>
2019-04-09 19:06:35 +0100

    cliente
    cliente only update

--- cliente/cliente/cliente.c
+++ cliente/cliente/cliente.c
@@ -10,89 +10,138 @@
 #define MAX 256
 
 void gotoxy(int x, int y);
-DWORD WINAPI Bola();
+DWORD WINAPI Bola(LPVOID param);
+DWORD WINAPI Barreira(LPVOID param);
 void hidecursor();
-								
+
+HANDLE stdoutMutex;
+
+typedef struct gameInfo {
+	DWORD limx, limy; //map info
+	DWORD baPosx, baPosy,baTam;//barreira info
+}data,*pdata;
+			
 int _tmain(int argc, LPTSTR argv[]) {
-	DWORD threadId, posx = 0, posy = 0, oposx, oposy,sizex,sizey;
-	_TCHAR KeyPress;
-	HANDLE hT;
+	DWORD threadId;
+	HANDLE hTBarreira;
 	//UNICODE: Por defeito, a consola Windows não processe caracteres wide.
 	//A maneira mais fácil para ter esta funcionalidade é chamar _setmode:
 #ifdef UNICODE
 	_setmode(_fileno(stdin), _O_WTEXT);
 	_setmode(_fileno(stdout), _O_WTEXT);
-#endif	
-
-	hT = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Bola,NULL, 0, &threadId);
-	WaitForSingleObject(hT, INFINITE);
-	return 0;
-
-	gotoxy(posx, posy);
-	_tprintf(TEXT("X"));
-	gotoxy(posx, posy);
-	while(1){
-		KeyPress = _getch();
-		oposx = posx;
-		oposy = posy;
-		//_putch(KeyPress);
-		switch (KeyPress) {
-			case 'a':
-			case 'A':
-				gotoxy(oposx, oposy);
-				_tprintf(TEXT(" "));
-				posx--;
-				gotoxy(posx,posy);
-				_tprintf(TEXT("X"));
-				gotoxy(posx, posy);
-				break;
-
-			case 'd':
-			case 'D':
-				gotoxy(oposx, oposy);
-				_tprintf(TEXT(" "));
-				posx++;
-				gotoxy(posx, posy);
-				_tprintf(TEXT("X"));
-				gotoxy(posx, posy);
-				break;
-
-			case 27:
-				gotoxy(0, 1);
-				_tprintf(TEXT("EXITING"));
-				return 0;
-				break;
-		}
-		
-	
-		
+#endif
+	pdata game = (pdata)malloc(sizeof(data));
+	if (!game) {
+		printf("Erro ao reserver memoria estrutura\n");
+		return -1;
 	}
-	
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+	game->limx = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+	game->limy = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+	stdoutMutex = CreateMutex(NULL, FALSE, NULL);
+	hidecursor();
+	hTBarreira = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Barreira, (LPVOID)game, 0, &threadId);
+	if (hTBarreira == NULL) {
+		_tprintf(TEXT("Erro ao criar thread Barreira"));
+		return -1;
+	}
+	WaitForSingleObject(hTBarreira, INFINITE);
+	free(game);
+	CloseHandle(stdoutMutex);
 	_tprintf(TEXT("[Thread Principal %d] Vou terminar..."),GetCurrentThreadId());
 	return 0;
 }
 
-DWORD WINAPI Bola() {
+DWORD WINAPI Barreira(LPVOID param) {
+	pdata gameInfo = ((pdata)param);
+	gameInfo->baTam = 15;
+	gameInfo->baPosx = gameInfo->limx / 2, gameInfo->baPosy = gameInfo->limy - 2;
+
+	WaitForSingleObject(stdoutMutex, INFINITE);
+	gotoxy(gameInfo->baPosx, gameInfo->baPosy);
+	for (int i = 0; i < gameInfo->baTam; i++) {
+		_tprintf(TEXT("O"));
+	}
+	ReleaseMutex(stdoutMutex);
+
+	TCHAR KeyPress;
+	HANDLE hTBola;
+	DWORD threadId;
+	while (1) {
+		KeyPress = _getch();
+		
+		//_putch(KeyPress);
+		switch (KeyPress) {
+		case 'a':
+		case 'A':
+		case 'K'://left arrow
+			if (gameInfo->baPosx <= 0)
+				break;
+			WaitForSingleObject(stdoutMutex,INFINITE);
+			gotoxy(gameInfo->baPosx + gameInfo->baTam - 1, gameInfo->baPosy);
+			_tprintf(TEXT(" "));
+			gameInfo->baPosx--;
+			gotoxy(gameInfo->baPosx, gameInfo->baPosy);
+			_tprintf(TEXT("O"));
+			ReleaseMutex(stdoutMutex);
+			break;
+
+		case 'd':
+		case 'D':
+		case 'M':
+		//right arrow
+			if (gameInfo->baPosx + gameInfo->baTam >= gameInfo->limx)
+				break;
+			WaitForSingleObject(stdoutMutex, INFINITE);
+			gotoxy(gameInfo->baPosx, gameInfo->baPosy);
+			_tprintf(TEXT(" "));
+			gameInfo->baPosx++;
+			gotoxy(gameInfo->baPosx + gameInfo->baTam-1, gameInfo->baPosy);
+			_tprintf(TEXT("O"));
+			ReleaseMutex(stdoutMutex);
+			break;
+		case 32:
+			WaitForSingleObject(stdoutMutex, INFINITE);
+			gotoxy(0, gameInfo->limy / 2);
+			for (int i = 0; i < gameInfo->limx; i++) {
+				_tprintf(TEXT(" "));
+			}
+			ReleaseMutex(stdoutMutex);
+			hTBola = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Bola, (LPVOID)gameInfo, 0, &threadId);
+			if (hTBola == NULL) {
+				_tprintf(TEXT("Erro ao criar thread Bola!"));
+				return -1;
+			}
+			break;
+
+		case 27://ESC
+			gotoxy(0, 1);
+			_tprintf(TEXT("EXITING"));
+			ExitThread(NULL);
+			break;
+		}
+	}
+}
+
+DWORD WINAPI Bola(LPVOID param) {
+	pdata gameInfo = ((pdata)param);
 	srand((int)time(NULL));
 	boolean goingUp = 1, goingRight = 1;
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-	DWORD limx = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-	DWORD limy = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-	DWORD posx = limx/2, posy = limy/2, oposx, oposy;
-	goingUp = (rand() % 2);
+	DWORD posx = gameInfo->limx/2, posy = gameInfo->limy/2, oposx, oposy;
+	//goingUp = (rand() % 2);
 	goingRight = (rand() % 2);
-	//_tprintf(TEXT("lim(%d,%d)"), limx, limy);
-	hidecursor();
+	WaitForSingleObject(stdoutMutex, INFINITE);
 	gotoxy(posx, posy);
-	_tprintf(TEXT("."));
-	_getch();
+	_tprintf(TEXT("B"));
+	ReleaseMutex(stdoutMutex);
 	while (1) {
-		Sleep(20);
+		//Sleep(30);
+		Sleep(100);
 		oposx = posx;
 		oposy = posy;
 		if (goingRight){
-			if (posx < limx-1) {
+			if (posx < gameInfo->limx-1) {
 				posx++;
 			}
 			else {
@@ -120,16 +169,31 @@ DWORD WINAPI Bola() {
 			}
 		}
 		else {
-			if (posy < limy - 1) {
-				posy++;
+			if (posy < gameInfo->limy - 1) {// se nao atinge o limite do mapa
+				if (posy == gameInfo->baPosy - 1 && (posx >= gameInfo->baPosx && posx <= (gameInfo->baPosx + gameInfo->baTam))) {//atinge a barreira
+					posy--;
+					goingUp = 1;
+				}else
+					posy++;
 			}
-			else {
+			else {//se atinge o fim do mapa
+				WaitForSingleObject(stdoutMutex, INFINITE);
+				gotoxy(gameInfo->limx/3, gameInfo->limy/2);
+				_tprintf(TEXT("Game Over! Press Space for another ball!\n"));
+				ReleaseMutex(stdoutMutex);
+				ExitThread(NULL);
 				posy--;
 				goingUp = 1;
 			}
 		}
 
-		/*gotoxy(5,5);
+
+
+		/*gotoxy(5, 4);
+		_tprintf(TEXT("Barreira entre:(%d,%d) at %d\n"),gameInfo->baPosx,gameInfo->baPosx + gameInfo->baTam,gameInfo->baPosy);
+		gotoxy(5, 5);
+		_tprintf(TEXT("Right:%d\n"), goingRight);
+		gotoxy(5,5);
 		_tprintf(TEXT("Right:%d\n"),goingRight);
 		gotoxy(5, 6);
 		_tprintf(TEXT("Up:%d\n"), goingUp);
@@ -138,10 +202,12 @@ DWORD WINAPI Bola() {
 		gotoxy(5, 8);
 		_tprintf(TEXT("posy:%d\n"), posy);*/
 
+		WaitForSingleObject(stdoutMutex, INFINITE);
 		gotoxy(oposx, oposy);
 		_tprintf(TEXT(" "));
 		gotoxy(posx, posy);
-		_tprintf(TEXT("."));
+		_tprintf(TEXT("B"));
+		ReleaseMutex(stdoutMutex);
 
 	}
 	
